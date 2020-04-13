4d80d83909d3984a96c3279f29ad6c099d6dea53
Hansel Junior Ospino Orellano <hansel.ospino@globant.com>
2020-04-13 12:44:08 -0500

    @ModelAttribute example.

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ Spring Framework: Spring MVC Fundamentals
 - @GetMapping
 - Update model
     - Map<String, String> model
+- @ModelAttribute
+- @PostMapping
+## About MVC
+### View
+View can access the model through a controller. View represents the model or desired data.
+### Controller
+It interprets the user requests and selects the proper view based of the requested view or what information we get back from our data model.
+### Model
+Model is not talking about database or the framework used to access to the database. The real thing is regarding the information that we are going to represent in the view.
---
+++ src/main/java/com/pluralsight/conferencedemomvc/controller/RegistrationController.java
@@ -0,0 +1,27 @@
+package com.pluralsight.conferencedemomvc.controller;
+
+import com.pluralsight.conferencedemomvc.model.Registration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Controller
+public class RegistrationController {
+
+    private static final Logger log = Logger.getLogger(RegistrationController.class.getName());
+
+    @GetMapping("registration")
+    public String getRegistration(@ModelAttribute(name = "registration")Registration registration) {
+        return "registration";
+    }
+
+    @PostMapping("registration")
+    public String addRegistration(@ModelAttribute(name = "registration")Registration registration) {
+        log.log(Level.INFO, "Registration: " + registration.getName());
+        return "registration";
+    }
+}
