d138e0664303b937efdbe1c675aafac93f3d2381
Hansel Junior Ospino Orellano <hansel.ospino@globant.com>
2020-04-12 19:46:01 -0500

    Initial commit

---
+++ src/main/java/com/pluralsight/conferencedemomvc/ConferenceDemoMvcApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.conferencedemomvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceDemoMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceDemoMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pluralsight/conferencedemomvc/ConferenceDemoMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.conferencedemomvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceDemoMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
