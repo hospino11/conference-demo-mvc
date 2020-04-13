a7eac8051d3a4ba0ffc59420eac5646df86c475b
Hansel Junior Ospino Orellano <hansel.ospino@globant.com>
2020-04-12 20:31:46 -0500

    MVC configuration for serving JSP.

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 Spring Framework: Spring MVC Fundamentals
 ## Topics covered
 - JAR to WAR
+- MVC Configuration
+    - SpringBootServletInitializer
+    - spring.mvc.view.prefix=/WEB-INF/jsp/
+    - spring.mvc.view.suffix=.jsp
+- @Controller
+- @GetMapping
+- Update model
+    - Map<String, String> model
--- src/main/java/com/pluralsight/conferencedemomvc/ConferenceDemoMvcApplication.java
+++ src/main/java/com/pluralsight/conferencedemomvc/ConferenceDemoMvcApplication.java
@@ -2,9 +2,10 @@ package com.pluralsight.conferencedemomvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ConferenceDemoMvcApplication {
+public class ConferenceDemoMvcApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConferenceDemoMvcApplication.class, args);
